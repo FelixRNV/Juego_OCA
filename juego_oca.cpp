32f6268d1a86fdae7041e1c484773e6dbdcc952c
Felix Navas <fnavas@est.ups.edu.ec>
2023-07-07 16:21:34 -0500

    Imagenes

--- juego_oca.cpp
+++ juego_oca.cpp
@@ -6,6 +6,12 @@ Juego_OCA::Juego_OCA(QWidget *parent)
     , ui(new Ui::Juego_OCA)
 {
     ui->setupUi(this);
+    ui->label->setStyleSheet("QLabel { background-color: red; color : blue; }");
+    QPalette palette = ui->label->palette();
+     palette.setColor(ui->label->backgroundRole(), Qt::yellow);
+     palette.setColor(ui->label->foregroundRole(), Qt::yellow);
+     ui->label->setPalette(palette);
+
 }
 
 Juego_OCA::~Juego_OCA()
