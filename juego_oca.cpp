716c0da004697a70c49271c9731e733ac957dbd6
Felix Navas <fnavas@est.ups.edu.ec>
2023-07-08 05:30:24 -0500

    Diseño Tablero

--- juego_oca.cpp
+++ juego_oca.cpp
@@ -6,11 +6,7 @@ Juego_OCA::Juego_OCA(QWidget *parent)
     , ui(new Ui::Juego_OCA)
 {
     ui->setupUi(this);
-    ui->label->setStyleSheet("QLabel { background-color: red; color : blue; }");
-    QPalette palette = ui->label->palette();
-     palette.setColor(ui->label->backgroundRole(), Qt::yellow);
-     palette.setColor(ui->label->foregroundRole(), Qt::yellow);
-     ui->label->setPalette(palette);
+    ui->widCasIni->setStyleSheet("background-color: rgba(0,0,0,0)");
 
 }
 
